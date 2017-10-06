856066841692f3cb9700e33d91b1f7e4ac065ade
David Atchley <david.m.atchley@gmail.com>
2017-10-06 11:49:39 -0500

    fix: dev and prod style generation
    - removed node-sass-chokidar and build/watch steps in package scripts
    - updated our @tandem.ly/react-scripts to latest version for relevant fixes there
    - ensured icon fonts and styles were present.
    - added some additional global style mods/helpers from @stu
    - removed all previously generated css files (not needed anymore, since we can import .s[ca]ss directly in to .js files)

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 // @flow
 import React from "react";
 import ReactDOM from "react-dom";
-import "./styles/index.css";
 import App from "./containers/App";
 import registerServiceWorker from "./registerServiceWorker";
+
+import "./styles/index.scss";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
